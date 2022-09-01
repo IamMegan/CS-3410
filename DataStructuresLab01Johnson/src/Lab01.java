0d73c26953c4bb2c505d20edc79a508856e92199
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-09-01 12:15:29 -0400

    Update Lab01.java

--- DataStructuresLab01Johnson/src/Lab01.java
+++ DataStructuresLab01Johnson/src/Lab01.java
@@ -1,9 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lab01 {
-	private int[][] matrix = new int[5][5];
+	
+	private int row;
+	private int col;
+	private int[][] matrix = new int[this.row][this.col];
 	Random rand = new Random();
 	
+	
+	
+	public void setRowCol(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
 	
 	public void populateMatrix() {
 		for(int i = 0; i < matrix.length; i++) {
@@ -31,18 +41,23 @@ public class Lab01 {
 					maxCol = matrix[j][i];
 				}
 			}
-			System.out.print("Col" + i + "max is:" + maxCol + "\n");
+			System.out.print("Col " + i + " max is: " + maxCol + "\n");
 		}
 	}
 	
 	public static void main(String[] args) {
 		Lab01 matrix = new Lab01();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a size: ");
+		int row = scan.nextInt();
+		int col = scan.nextInt();
 		
+		matrix.setRowCol(row, col);
 		matrix.populateMatrix();
 		matrix.printMatrix();
 		matrix.maxCol();
+		scan.close();
 	}
 	
 }
 	
-
